e59f3dc9e5878e9d345780715bd2b974d41a9b74
Romain ODEVAL <romain.odeval@protonmail.com>
2018-09-09 17:03:14 +0200

    WIP: Web admin for future "Annoucements" plugin

---
+++ src/Hedgebot/Plugin/AnnouncementsBundle/Controller/AnnouncementsController.php
@@ -0,0 +1,68 @@
+<?php
+namespace Hedgebot\Plugin\AnnouncementsBundle\Controller;
+
+use Hedgebot\CoreBundle\Controller\BaseController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class AnnouncementsController extends BaseController
+{
+    /**
+     * Hook that is executed before the action is called.
+     * Mainly, binds the breadcrumb for the whole controller.
+     */
+    public function beforeActionHook()
+    {
+        parent::beforeActionHook();
+
+        $breadcrumbs = $this->get('white_october_breadcrumbs');
+        $breadcrumbs->addItem("Announcements", $this->generateUrl("announcements_list"));
+    }
+
+    /**
+     * @Route("/announcements", name="announcements_list")
+     */
+    public function announcementsListAction(Request $request)
+    {
+        $templateVars = [];
+
+        $endpoint = $this->get('hedgebot_api')->endpoint('/plugin/announcements');
+        $serverEndpoint = $this->get('hedgebot_api')->endpoint('/server');
+        $templateVars['messages'] = $endpoint->getMessages();
+        $templateVars['availableChannels'] = $serverEndpoint->getChannels();
+
+        return $this->render('HedgebotAnnouncementsBundle::route/index.html.twig', $templateVars);
+    }
+
+    /**
+     * @Route("/announcements/message/delete/{name}", options = { "expose" = true }, name="announcements_message_delete")
+     */
+    public function deleteMessageAction($name)
+    {
+        $endpoint = $this->get('hedgebot_api')->endpoint('/plugin/announcements');
+        $deleted = $endpoint->deleteMessage($name);
+
+        $response = new JsonResponse();
+        $response->setData($deleted);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/announcements/message/save/{name}", options = { "expose" = true }, name="announcements_message_save")
+     */
+    public function saveMessageAction($name, Request $request)
+    {
+        $endpoint = $this->get('hedgebot_api')->endpoint('/plugin/announcements');
+        $data = $request->request->all();
+
+        $updated = $endpoint->saveMessage($name, $data);
+
+        $response = new JsonResponse();
+        $response->setData($updated);
+
+        return $response;
+    }
+}
---
+++ web/js/modules/plugin/announcements.js
@@ -0,0 +1,222 @@
+var Announcements = {
+
+    defaultOptions: {
+        /**
+         * @var string Selector pointing to the messages list container
+         */
+        messageContainerSelector: null,
+
+        /**
+         * @var string Selector for one message, inside the message container
+         */
+        messageSelector: null,
+
+        /**
+         * @var string Selector to the message template element
+         */
+        messageTemplateSelector: null,
+
+        /**
+         * @var string The selector for the "add message" button
+         */
+        addMessageSelector: null,
+
+        /**
+         * @var string Route to delete a message on the controller
+         */
+        deleteMessageRoute: null,
+
+        /**
+         * @var string Route to save a message on the controller
+         */
+        saveMessageRoute: null,
+
+        /**
+         * @var object Field names for each value
+         */
+        fieldNames: {
+            message: null,
+            channels: null
+        }
+    },
+
+    options: {},
+    elements: {
+        commandContainer: null,
+        addButton: null,
+        commandTemplate: null
+    },
+
+    /**
+     * Initializes the module
+     * 
+     * @param array options The option list
+     */
+    init: function(options)
+    {
+        this.options = $.extend(this.defaultOptions, options);
+
+        this.initElements();
+        this.bindUIActions();
+    },
+
+    /**
+     * Initializes the elements.
+     */
+    initElements: function(refresh)
+    {
+        this.elements.commandContainer = $(this.options.commandContainerSelector);
+        this.elements.addButton = $(this.options.addSelector);
+        this.elements.commandTemplate = $(this.options.commandTemplateSelector);
+    },
+
+    /**
+     * Binds events from the elements to callbacks
+     */
+    bindUIActions: function()
+    {
+        this.elements.commandContainer.on('click', '[data-action="save"]', this.onSaveCommandClick.bind(this));
+        this.elements.commandContainer.on('click', '[data-action="delete"]', this.onDeleteCommandClick.bind(this));
+
+        this.elements.addButton.on('click', this.onAddCommandClick.bind(this));
+    },
+
+    /// EVENTS ///
+
+    /**
+     * Event: the Add command button has been pressed
+     */
+    onAddCommandClick: function()
+    {
+        this.addCommand();
+    },
+
+    /**
+     * Event: A delete command button has been pressed
+     */
+    onDeleteCommandClick: function(ev)
+    {
+        var commandBlock = this.elements.commandContainer.find(ev.currentTarget.dataset.target).not();
+
+        // If this is an already saved command, there should be a valid target
+        if(commandBlock.length > 0)
+            this.deleteCommand(commandBlock.data('name'), this.onCommandDeletedResult.bind(this, commandBlock));
+        else // This is a new, not-yet-saved command, we just delete the matching parent command element
+            $(ev.currentTarget).parents(this.options.commandSelector).remove();
+
+    },
+    
+    /**
+     * Event: A command has been deleted.
+     */
+    onCommandDeletedResult: function(commandBlock, success)
+    {
+        // Deletion has been successful, we remove the command block
+        if(success) {
+            commandBlock.remove();
+            $.notify({ message: "Command has been deleted." });
+        }
+        else
+            $.notify({ message: "An error occured during command deletion." }, { type: "danger" });
+    },
+
+    /**
+     * Event: Saves a command
+     */
+    onSaveCommandClick: function(ev)
+    {
+        var commandBlock = this.elements.commandContainer.find(ev.currentTarget.dataset.target);
+        var commandData = {};
+        var commandName = null;
+
+        // If the command block has not been found, then it's a new command and just find the parent block that matches the command selector
+        if(commandBlock.length == 0)
+            commandBlock = $(ev.currentTarget).parents(this.options.commandSelector);
+
+        commandName = commandBlock.data('name');
+
+        commandData.name = commandBlock.find('[name="' + this.options.fieldNames.name + '"]').val();
+        commandData.text = commandBlock.find('[name="' + this.options.fieldNames.text + '"]').val();
+        commandData.channels = commandBlock.find('[name="' + this.options.fieldNames.channels + '"]').val();
+        
+        // If it's a new command, the commandName var will not be filled, so we'll fetch it from the actual input
+        if(!commandName)
+            commandName = commandData.name;
+
+        this.saveCommand(commandName, commandData, this.onCommandSavedResult.bind(this, commandBlock, commandData));
+    },
+
+    /**
+     * Event: A command has been saved.
+     */
+    onCommandSavedResult: function(commandBlock, commandData, success)
+    {
+        // If the save has succeeded, we need to update the block with the new ID and command name, and their references
+        if(success) {
+            commandBlock.data('name', commandData.name);
+            commandBlock.attr('data-name', commandData.name);
+            commandBlock.attr('id', 'command-' + commandData.name);
+
+            commandBlock.find('[data-action="save"]').attr('data-target', "#command-" + commandData.name);
+            commandBlock.find('[data-action="delete"]').attr('data-target', "#command-" + commandData.name);
+
+            commandBlock.removeClass('not-saved');
+            $.notify({ message: "Command has been saved." });
+        }
+        else
+            $.notify({ message: "An error occured during command save." }, { type: "danger" });
+    },
+
+    /// ACTIONS ///
+
+    saveCommand: function(commandName, commandData, callback)
+    {
+        $.ajax({
+            url: Routing.generate(this.options.saveRoute, {name: commandName}, true),
+            type: 'post',
+            data: commandData,
+            dataType: 'json',
+            complete: function(jqXHR, textStatus)
+            {
+                var data = jqXHR.responseJSON;
+                callback(textStatus == "success" && data === true);
+            }
+        });
+    },
+
+    deleteCommand: function(commandName, callback)
+    {
+        $.ajax({
+            url: Routing.generate(this.options.deleteRoute, {name: commandName}, true),
+            type: 'get',
+            dataType: 'json',
+            complete: function(jqXHR, textStatus)
+            {
+                var data = jqXHR.responseJSON;
+                callback(textStatus == "success" && data === true);
+            }
+        });
+    },
+
+    addCommand: function()
+    {
+        var newCommandBlock = this.elements.commandTemplate.clone();
+        newCommandBlock.addClass('not-saved');
+        newCommandBlock.attr("id", "#command-" + this.makeID(10)); // Set the ID to a random one
+
+        $('select', newCommandBlock).removeClass('ms').selectpicker();
+
+        this.elements.commandContainer.append(newCommandBlock);
+    },
+
+    makeID: function(length)
+    {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      
+        for (var i = 0; i < length; i++)
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+      
+        return text;
+    }
+};
