b3163f8043171c4b66c170343d3f9a9de1415177
Yohann Lorant <yohann.lorant@gmail.com>
2020-06-02 06:16:25 +0200

    Updated Hedgebot API client to use it as external library.

--- src/Hedgebot/CoreBundle/Service/ApiClientService.php
+++ src/Hedgebot/CoreBundle/Service/ApiClientService.php
@@ -1,35 +1,8 @@
 <?php
 namespace Hedgebot\CoreBundle\Service;
 
-use Hedgebot\CoreBundle\API\Client;
+use HedgebotApi\Client as HedgebotApiClient;
 
-class ApiClientService
+class ApiClientService extends HedgebotApiClient
 {
-    private $uri;
-    
-    /**
-     * Builds an API client for the given API base URI
-     */
-    public function __construct($uri, $token = null)
-    {
-        $this->client = new Client();
-        $this->client->setBaseUrl($uri);
-        
-        if (!empty($token)) {
-            $this->client->setAccessToken($token);
-        }
-    }
-    
-    /**
-     * Sets the endpoint and gets a new Client instance.
-     * @param  string $name The endpoint name.
-     * @return Client       A new client.
-     */
-    public function endpoint($name)
-    {
-        $client = clone $this->client;
-        $client->setEndpoint($name);
-
-        return $client;
-    }
 }
