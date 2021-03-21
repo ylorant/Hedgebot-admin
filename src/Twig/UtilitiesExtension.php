20915fa6c874500339c0417a0be366c7d4f10593
Romain Odeval <romain.odeval@protonmail.com>
2021-03-21 20:28:03 +0100

    symfony4-upgrade
    * Fix intl usages with Twig
    * Fix last deprecation alerts (job done to prepare Symfony 5.x upgrade)

--- src/Twig/UtilitiesExtension.php
+++ src/Twig/UtilitiesExtension.php
@@ -11,7 +11,7 @@ class UtilitiesExtension extends AbstractExtension
     /**
      * {@inheritdoc}
      */
-    public function getTests()
+    public function getTests(): array
     {
         return [
             new TwigTest('instanceof', [$this, 'isInstanceOf'])
@@ -21,7 +21,7 @@ class UtilitiesExtension extends AbstractExtension
     /**
      * {@inheritdoc}
      */
-    public function getFilters()
+    public function getFilters(): array
     {
         return [
             new TwigFilter('anonymize', [$this, 'anonymize']),
@@ -31,45 +31,45 @@ class UtilitiesExtension extends AbstractExtension
         ];
     }
 
-    public function isInstanceOf($object, $class)
+    public function isInstanceOf($object, $class): bool
     {
         $reflectionClass = new \ReflectionClass($class);
         return $reflectionClass->isInstance($object);
     }
 
-    public function anonymize($input)
+    public function anonymize($input): string
     {
         $charactersToShow = strlen($input) > 16 ? 4 : ceil(strlen($input) / 4);
         return substr($input, 0, $charactersToShow) . '***' . substr($input, -1 * $charactersToShow);
     }
 
-    public function toArray($input)
+    public function toArray($input): array
     {
         return (array) $input;
     }
 
     /**
      * Twig filter: Generates an icon from its key
+     * @param $input
+     * @return string
      */
-    public function iconize($input)
+    public function iconize($input): string
     {
         $iconParts = explode(':', $input, 2);
-// Default type: material_icons
+        // Default type: material_icons
         if (count($iconParts) == 1) {
             array_unshift($iconParts, 'material-icons');
         }
 
         $classes = $iconParts[0];
         $content = '';
-// Handling all types of icons that the admin is able to generate
+        // Handling all types of icons that the admin is able to generate
         switch ($iconParts[0]) {
             case 'zmdi':
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 $classes .= ' zmdi-' . $iconParts[1];
-
+                $classes .= ' zmdi-' . $iconParts[1];
                 break;
             case 'material-icons':
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 $content = $iconParts[1];
-
+                $content = $iconParts[1];
                 break;
         }
 
