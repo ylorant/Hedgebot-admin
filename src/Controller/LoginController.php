3e42cda4efdef0f5ff3abc2172671f266390d4cc
Yohann Lorant <yohann.lorant@gmail.com>
2022-03-07 17:01:51 +0100

    Rename SecurityController into LoginController (as it's the only thing it does)

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,7 +6,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends BaseController
+class LoginController extends BaseController
 {
     /**
      * @Route("/login", name="login")
